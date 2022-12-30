258ad8630d186138f3bd627cc65531c250f6c460
Jesus Angulo <jangulo@baufest.com>
2022-12-30 17:44:12 -0500

    add config sidecar

---
+++ 01-single-node/1-sidecar/02-config-ambassador/regular-webapp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using regular_webapp.Models;
+
+namespace regular_webapp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Setting = _configuration.GetValue<string>("Title");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ 01-single-node/1-sidecar/02-config-ambassador/regular-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace regular_webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
